1653fb827085d9fdfef6484b189cc89f888501a0
Vijay Daita <hello@daita.com>
2023-06-26 13:02:46 -0500

    added further stripe information

--- src/pages/api/payment.ts
+++ src/pages/api/payment.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import stripe from 'stripe';
 
-const stripeInstance = new stripe("", {
+const stripeInstance = new stripe("pk_test_51Kb55ZK7c7Mb50VzqCqKpw8CKE2OaOaN6dXX9CSFOESTYCO8XzzYAyR3AKfy1T2wdh246mwmWc1xHDW0MxUQej6j00gzQGymvF", {
     apiVersion: '2022-11-15'
 });
 
@@ -10,11 +10,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const supabaseId = req.body.supabaseId;
     const session = await stripeInstance.checkout.sessions.create({
         mode: 'payment',
-        success_url: '',
-        cancel_url: '',
+        success_url: 'https://localhost:3000/checkout_completed/success',
+        cancel_url: 'https://localhost:3000/checkout_completed/failure',
         line_items: [
             {
-                price: '',
+                price: 'price_1NNHfLK7c7Mb50VzYRrAy30o',
                 quantity: 1
             }
         ],
