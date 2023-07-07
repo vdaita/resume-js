6b9057c42254c45ad3ff671261ac07000f543e1c
Vijay Daita <hello@daita.com>
2023-07-06 22:34:20 -0400

    created free trial

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -35,6 +35,8 @@ export default function Home() {
   const [userLoggedIn, setUserLoggedIn] = useState(false);
   const [user, setUser] = useState(false);
 
+  const [freeTrialUsed, setFreeTrialUsed] = useState(false);
+         
   useEffect(() => {
     // console.log("stripe public: ", STRIPE_PUBLIC);
     supabase.auth.onAuthStateChange((event, session) => {
@@ -44,14 +46,34 @@ export default function Home() {
         setPasswordRecovery(true);
       }
       if(session){
+        console.log("Current session user: ", session.user, session.user.email);
         setUser(session.user);
+        isFreeTrialUsed();        
         setUserLoggedIn(true);
       } else {
         setUser(undefined);
+        setFreeTrialUsed(false);
         setUserLoggedIn(false);
       }
     })
   }, []);
+
+  let isFreeTrialUsed = async () => {
+    const {data, error} = await supabase.from("requests").select().eq("watermarked_free_trial", true).eq("id", user.id);
+    if(error){
+      toast({
+        title: 'Error checking your free trial status.',
+        description: '',
+        status: 'error',
+        duration: 2000,
+        isClosable: true
+      });
+      return false;
+    } else {
+      setFreeTrialUsed(data.length > 0);
+      return data.length > 0;
+    }
+  }
 
   let generateBucketId = async (strLength) => {
 
@@ -107,7 +129,7 @@ export default function Home() {
     return true;
   }
 
-  let createNewRecordInSupabase = async(bucketId, prompts) => {
+  let createNewRecordInSupabase = async(bucketId, prompts, isWatermarked) => {
       const {data: {user}} = await supabase.auth.getUser();
       const {data, error} = await supabase
         .from("requests")
@@ -115,6 +137,7 @@ export default function Home() {
           id: bucketId,
           user_id: user.id,
           email: user.email,
+          watermarked_free_trial: isWatermarked,
           prompts: {"data": prompts.split("\n")}
         });
       if(error){
@@ -225,13 +248,11 @@ export default function Home() {
     return ["hsl(",hue,",100%,50%)"].join("");
   }
 
-  let uploadThenCheckout = async () => {
-
-    // first, validate the forms
-    if(images.length < 5 || prompts.length <= 0){
-      console.log(imageUrls, imageUrls.length, prompts.length)
+  let uploadFreeTrial = async () => {
+    const wasFreeTrialUsed = await isFreeTrialUsed();
+    if(wasFreeTrialUsed){
       toast({
-        title: 'Please fill out all parts of the form before submitting. You may need to add more images.',
+        title: 'Your free trial has already been used.',
         description: '',
         status: 'error',
         duration: 2000,
@@ -240,30 +261,66 @@ export default function Home() {
       return false;
     }
 
-    setLoading(true);
+    const didUploadWork = await upload(true);
 
-    // first, upload the images
-    let bucketId = await generateBucketId(7);
-    let uploadWorked = await uploadFilesToSupabase(bucketId, images);
-    if(!uploadWorked){
-      setLoading(false);
+    if(didUploadWork){
+      setFreeTrialUsed(true);
+      // // update free trial to having been used up
+      // const {data, error} = await supabase.auth.updateUser({
+      //   data: {freeTrialUsed: true}
+      // });
+      // setFreeTrialUsed(true);
+      // console.log(data, error);
+    }
+    setLoading(false);
+  }
+
+  let upload = async (isWatermarked) => {
+      // first, validate the forms
+      if(images.length < 5 || prompts.length <= 0){
+        console.log(imageUrls, imageUrls.length, prompts.length)
+        toast({
+          title: 'Please fill out all parts of the form before submitting. You may need to add more images.',
+          description: '',
+          status: 'error',
+          duration: 2000,
+          isClosable: true
+        })
+        return false;
+      }
+  
+      setLoading(true);
+  
+      // first, upload the images
+      let bucketId = await generateBucketId(7);
+      let uploadWorked = await uploadFilesToSupabase(bucketId, images);
+      if(!uploadWorked){
+        setLoading(false);
+        return false;
+      }
+      // then, save the other parts of the request
+      console.log("prompts: ", prompts);
+      let recordCreationWorked = await createNewRecordInSupabase(bucketId, prompts, isWatermarked);
+      if(!recordCreationWorked){
+        setLoading(false);
+        return false;
+      }
+  
+      toast({
+        title: 'File upload successful!',
+        description: 'Your files were successfully uploaded for processing',
+        status: 'success',
+        duration: 2000,
+        isClosable: true
+      })
+      return bucketId;
+  }
+
+  let uploadThenCheckout = async () => {
+    let bucketId = await upload(false);
+    if(!bucketId){
       return;
     }
-    // then, save the other parts of the request
-    console.log("prompts: ", prompts);
-    let recordCreationWorked = await createNewRecordInSupabase(bucketId, prompts);
-    if(!recordCreationWorked){
-      setLoading(false);
-      return;
-    }
-
-    toast({
-      title: 'File upload successful!',
-      description: 'Your files were successfully uploaded for processing',
-      status: 'success',
-      duration: 2000,
-      isClosable: true
-    })
 
     // then, send the user to the checkout flow
     const checkoutProcessSucceeded = await processCheckout(bucketId);
@@ -331,17 +388,16 @@ export default function Home() {
             {user && 
               <Box>
                 {/* <HStack> */}
-                  {user.email}
+                  {user.email} | {freeTrialUsed ? 'Free trial used' : ''}
                   <Button backgroundColor="red.200" margin={2} color="white" onClick={() => supabase.auth.signOut()}>Sign out</Button>
                 {/* </HStack> */}
               </Box>
             }
           </Flex>
         
-          <Heading>petform</Heading>
-
           {!userLoggedIn &&
             <div>
+              <Heading>petform</Heading>
               <label>Please log in or sign up before submitting your form.</label>
               <Auth
               supabaseClient={supabase}
@@ -354,9 +410,10 @@ export default function Home() {
           {userLoggedIn &&
             <div>
               <VStack>
+                <Heading>petform</Heading>
                 <label>Select 5-10 photos of your pet, preferably from different angles and without any other subjects.</label>
                 <Box backgroundColor={getColor(images.length >= 10 ? 0 : (10 - images.length)/10)} textColor={"#000000"} borderRadius={3} padding={3}>
-                  <p>{images.length <= 5 ? ((5 - images.length).toString() + " images remaining") : "You can add up to 10 images."}</p>
+                  <p>{images.length < 5 ? ((5 - images.length).toString() + " images remaining") : "You can add up to 10 images."}</p>
                 </Box>
                 <Input type="file" multiple accept="image/*" onChange={onImagesAdd} value={[]}/>
               </VStack>
@@ -365,19 +422,25 @@ export default function Home() {
 
               <SimpleGrid columns={3} spacing={10}>
                 {imageUrls.map((item, index) => (
-                  <VStack marginTop={"4"}>
-                    <Image src={item} width={250} height={250}/>
-                    <Button backgroundColor="red.300" color="black" onClick={() => removeImage(index)}>remove image</Button>
+                  <VStack marginTop={"4"} borderWidth={2} borderRadius={4} padding={4}>
+                    <Image style={{objectFit: "cover"}} src={item} sizes="" width={200} height={200}/>
+                    <HStack flex={1}>
+                      <Button style={{alignSelf: "flex-end"}} bottom="0rem" backgroundColor="red.300" color="black" onClick={() => removeImage(index)}>remove image</Button>
+                    </HStack>
                   </VStack>
                 ))}
               </SimpleGrid>
 
               <VStack marginTop="8">
-                <label>Prompts</label>
+                <label>Prompts - what do you want the generated images to look like?</label>
                 <Textarea type="text" value={prompts} placeholder="my pet sitting majestically on a mountain, my pet in a suit" width='100%' onChange={(event) => setPrompts(event.target.value)}></Textarea>
-                {!loading && <Button marginTop="4" onClick={() => uploadThenCheckout()}>Proceed to checkout (via Stripe)</Button>}
-                {loading && <Spinner/>}
-                {paymentFailed && <a href={"mailto:vijay@longlaketech.com?subject=Petform Payment Failed&body=Payment code " + paymentFailed}>Email support with code (send as is)</a>}
+                <HStack>
+                  {(!loading && !freeTrialUsed) && <Button marginTop="4"  onClick={() => uploadFreeTrial()}>Use your watermarked free trial</Button>}
+                  {!loading && <Button marginTop="4" backgroundColor='blue.300' onClick={() => uploadThenCheckout()}>Proceed to checkout (via Stripe)</Button>}
+                  {loading && <Spinner/>}
+                </HStack>
+
+                {paymentFailed && <a color='blue.300' href={"mailto:vijay@longlaketech.com?subject=Petform Payment Failed&body=Payment code " + paymentFailed}>Email support with code (send as is)</a>}
               </VStack>
 
 
