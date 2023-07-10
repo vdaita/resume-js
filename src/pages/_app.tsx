79343aaf8610dacf0364fa84dc8c748cbbc75c71
Vijay Daita <vdaita@gmail.com>
2023-07-10 17:15:20 -0500

    meta pixel fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,25 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
+import Script from 'next/script';
 import { Router } from 'next/router';
 import Head from 'next/head';
 
-function FacebookPixel(){
-  React.useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init("257397480350606");
-        ReactPixel.pageView();
+// function FacebookPixel(){
+//   React.useEffect(() => {
+//     import("react-facebook-pixel")
+//       .then((x) => x.default)
+//       .then((ReactPixel) => {
+//         ReactPixel.init("257397480350606");
+//         ReactPixel.pageView();
 
-        Router.events.on("routeChangeComplete", () => {
-          ReactPixel.pageView();
-        });
-      });
-  });
-  return null;
-}
+//         Router.events.on("routeChangeComplete", () => {
+//           ReactPixel.pageView();
+//         });
+//       });
+//   });
+//   return null;
+// }
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -29,8 +30,21 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="description" content="Generate pet photo" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <Script>
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '257397480350606');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </Head>
-      <FacebookPixel/>
       <ChakraProvider>
         <Component {...pageProps}/>
       </ChakraProvider>
--- src/pages/cksuccess.js
+++ src/pages/cksuccess.js
@@ -4,19 +4,34 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Button, ButtonGroup, Textarea, VStack, HStack, Link, Input, Heading, Text, useToast} from '@chakra-ui/react';
+import Script from "next/script";
 
 export default function Success() {
     return (
-        <main className={styles.main}>
-            <VStack>
-                <Link href="/">
-                    <Heading margin={4}>← petform</Heading>
-                </Link>
-                <p>
-                Your payment was successful! You should expect an email from longlaketech.com in the coming days with either your images or a request for clarification.
-                </p>
-            </VStack>
-           
-        </main>
+        <>
+            <Script>
+                {`!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '257397480350606');
+            fbq('track', 'StartTrial');`}
+            </Script>
+            <main className={styles.main}>
+                <VStack>
+                    <Link href="/">
+                        <Heading margin={4}>← petform</Heading>
+                    </Link>
+                    <p>
+                    Your payment was successful! You should expect an email from longlaketech.com in the coming days with either your images or a request for clarification.
+                    </p>
+                </VStack>
+            </main>
+        </>
+
     )
 }
