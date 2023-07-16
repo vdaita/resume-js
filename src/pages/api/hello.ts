32cda90c86c634c430d0f998a93d0d9d69898db2
Vijay Daita <hello@daita.com>
2023-07-16 18:33:56 -0400

    v1

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -7,7 +7,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_SECRET_KEY
 });
 const openai = new OpenAIApi(configuration);
 
@@ -20,11 +20,18 @@ const handler = async (
   res: NextApiResponse<Data>
 ) => {
   try{
+
+    console.log("Request: ", req.body.toString().substring(0, 100));
+
     let fileContent = req.body.resume;
-    let jobDesc = req.body.jobDesc;
+    let jobDesc = req.body.job;
+    let outputType = req.body.outputType;
   
     // send information to the HuggingFace/OpenAI API
-    if(fileContent && jobDesc){
+    if(fileContent.length > 0 && jobDesc.length > 0){
+
+      console.log("both fileContent and jobDesc exist");
+
       let prompt = `JOB DESCRIPTION:
 
       ${jobDesc}
@@ -35,21 +42,25 @@ const handler = async (
       
       INSTRUCTION:
       
-      You are an expert resume writer with many years of experience helping people get jobs and working within HR departments. Generate a highly effective resume and a cover letter each in LaTeX format based on the above current resume and optimized to get hired for the above job description. Do not use any LaTeX extensions or fonts that don't come standard. Do not list anything on the new resume that's not based on content from the above current resume. The resume must be capable of getting past HR Applicant Tracking System filters.`
+      You are an expert resume writer with many years of experience helping people get jobs and working within HR departments. Generate a highly effective ${outputType} each in Markdown format based on the above current resume and optimized to get hired for the above job description. Do not use any Markdown extensions or fonts that don't come standard. Do not list anything on the new resume that's not based on content from the above current resume. The resume must be capable of getting past HR Applicant Tracking System filters.`
 
       const chatCompletion = await openai.createChatCompletion({
-        model: "gpt-4.0",
+        model: "gpt-3.5-turbo",
         messages: [{role: "user", content: prompt}]
       });
 
+      console.log("Chat completed: ", chatCompletion.data);
+
       res.status(200).send({message: chatCompletion.data.choices[0].message?.content});
     } else {
+      console.error("Sent incomplete input");
       res.status(500).send({message: "Incomplete input"});
     }
 
     // using social logins instead of email will probably reduce shareability significantly
   } catch (err) {
-    res.status(400).send({message: 'Bad request'});
+    console.error("Exception", err.toJSON());
+    res.status(400).send({message: 'Error'});
   }
 }
 
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,14 @@
 'use client'
 
-import 'katex/dist/katex.min.css';
+// import 'katex/dist/katex.min.css';
 import React, {useEffect, useRef, useState} from 'react';
 import { Button, ButtonGroup, Textarea, VStack, HStack, Box, Link, Input, Heading, Text, Container, useToast, Spinner, GlobalStyle} from '@chakra-ui/react';
-import Latex from 'react-latex-next';
+// import Latex from 'react-latex-next';
 import mammoth from 'mammoth/mammoth.browser';
+import ReactMarkdown from 'react-markdown';
+import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
+// import html2pdf from 'html2pdf.js/dist/html2pdf.min'
+import ReactToPrint, { useReactToPrint } from 'react-to-print';
 
 export default function Index() {
 
@@ -17,86 +21,128 @@ export default function Index() {
   const [loading, setLoading] = useState(false);
 
   const [resultFocus, setResultFocus] = useState(false);
-
-  const latexBox = useRef(null);
   
   const [result, setResult] = useState("");
+
+  // const handlePrint = () => {
+  //   console.log(markdownBox, markdownBox.current);
+  //   useReactToPrint({
+  //     content: () => markdownBox.current
+  //   })
+  // }
+
+  const handlePrint = useReactToPrint({
+    content: () => markdownBoxRef.current
+  });
+
+  let showCurrent = () => {
+    console.log(markdownBoxRef.current, markdownBoxRef);
+  }
 
   let onFileAdd = async (e) => {
     if(e.target.files.length > 0){
       setFile(e.target.files[0]);
       let targetFile = e.target.files[0];
       if(targetFile.name.split(".").at(-1) === "pdf"){
-        const url = URL.createObjectURL(targetFile.blob);
+        const url = URL.createObjectURL(targetFile.blob());
         
       } else {
         // dealing with a .docx
         console.log(targetFile);
-        let arrayBuf = await new Response(targetFile.blob).arrayBuffer();
+        const blob = new Blob([targetFile], {type: targetFile.type});
+        console.log(blob);
+        let arrayBuf = await new Response(blob).arrayBuffer();
         console.log(arrayBuf);
-        let rawText = await mammoth.extractRawText({arrayBuffer: arrayBuf});
+        let rawText = (await mammoth.extractRawText({arrayBuffer: arrayBuf})).value;
+        console.log(rawText);
         setResume(rawText);
       }
     }
   }
 
-  let query = async () => {
+  let query = async (outputType) => {
     setLoading(true);
-    let res = await fetch("/api/", {
+    let res = await fetch("/api/hello", {
       method: "POST",
       body: JSON.stringify({
         resume: resume,
-        job: desc        
+        job: desc,
+        type: outputType        
       }),
       headers: new Headers({
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       })
     });
+
+    let ret = await res.json();
     
-    setResult(res.body.message);
+    console.log("Response received: ", res, res.body, ret);
+
+    setResult(ret.message);
 
     setLoading(false);
   }
+
+  // https://dev.to/kazmi066/converting-jsx-to-downloadable-pdf-in-react-20bh
+  // const printHandler = () => {
+  //   const printElement = markdownBox.current;
+  //   html2pdf().from(printElement).save()
+  // }
 
   // paid feature: allow people to upload multiple job descriptions at the same time?
   // let save = async() => {
     
   // }
 
+  let RenderedMarkdown = () => (
+    <div>
+      <ReactMarkdown components={ChakraUIRenderer()} children={result}></ReactMarkdown> 
+    </div>
+  )
+
 
   return (
-    <main>
+    <main >
       {/* <Container alignChildren="center">
       </Container> */}
       <Container alignchildren="center">
-        {!resultFocus && 
-          <div className="noprint">
-            <Heading>Resume.js</Heading>
-            <Box padding={2} flex={1}>
-              <Text fontSize="lg">Current Resume</Text>
-              <Input ref={inputBtnRef} style={{display: 'none'}} type="file" multiple accept="application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document" onChange={onFileAdd} value={[]}/>
-              <Button colorScheme='teal' variant='solid' onClick={() => inputBtnRef.current.click()}>
-                Select file
-              </Button>
-              <Text>{file ? file.filename : ""}</Text>
-              <Text fontSize="sm">.doc, or .docx accepted</Text>
+        <div className="print-hide">
+          <Heading>Resume.js</Heading>
+          <Box padding={2} flex={1}>
+            <Text fontSize="lg">Current Resume</Text>
+            <Input ref={inputBtnRef} style={{display: 'none'}} type="file" multiple accept="application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document" onChange={onFileAdd} value={[]}/>
+            <Button colorScheme='teal' variant='solid' onClick={() => inputBtnRef.current.click()}>
+              Select file
+            </Button>
+            <Text>{file ? file.filename : ""}</Text>
+            <Text fontSize="sm">.doc, or .docx accepted</Text>
 
-              <Textarea flex={1} placeholder={"Copy and paste your resume manually or upload a file"} height={"400"} value={resume} onChange={(e) => setResume(e.target.value)}>
-              </Textarea>
-            </Box>
-            <Box padding={2}>
-              <Text fontSize="lg">Job description</Text>
-              
-              <Textarea onChange={(e) => setDesc(e.target.value)} value={desc}>
-              </Textarea>
-            </Box>
-            {!loading && <Button onClick={() => query()}>Submit</Button>}
-            {loading && <Spinner/>}
-            <Text>CTRL-P/CMD-P will show only the result.</Text>
-          </div>
-        }
-        <Latex ref={latexBox}>{result}</Latex> 
+            <Textarea flex={1} placeholder={"Copy and paste your resume manually or upload a file"} height={"400"} value={resume} onChange={(e) => setResume(e.target.value)}>
+            </Textarea>
+          </Box>
+          <Box padding={2}>
+            <Text fontSize="lg">Job description</Text>
+            
+            <Textarea onChange={(e) => setDesc(e.target.value)} value={desc}>
+            </Textarea>
+          </Box>
+          {!loading && <Button margin={2} onClick={() => query("resume")}>Generate Resume</Button>}
+          {!loading && <Button margin={2} onClick={() => query("cover letter")}>Generate Cover Letter</Button>}
+          {loading && <Spinner/>}
+          <br/>
+          <Box padding={2}>
+            <Text fontSize="lg">Edit Result (Markdown)</Text>
+            <Textarea height={"400"}  onChange={(e) => setResult(e.target.value)} value={result}/>
+            {result.length > 0 && <Button margin={2} onClick={() => window.print()}>Print</Button>}
+            {/* {result.length > 0 && <ReactToPrint
+              trigger={() => <Button margin={2}>Print</Button>}
+              content={() => markdownBox.current}/>} */}
+          </Box>
+          
+        </div>
+
+        <RenderedMarkdown/>
       </Container>
     </main>
   )
